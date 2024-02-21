e5b772c50de5ed4e91c9728fa9861b168cbf3c8a
a13n20 <143044296+a13n20@users.noreply.github.com>
2024-02-21 11:53:23 -0800

    Update test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -21,4 +21,4 @@ jobs:
       - uses: actions/checkout@v3
       - name: Build project
         run: g++ main.cpp --std=c++17 -o Palette
-./Palette 000 afC940 876 5104 FEFED 9964Ba 00G000
+        executable: ./Palette 000 afC940 876 5104 FEFED 9964Ba 00G000
