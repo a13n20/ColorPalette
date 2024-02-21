1ac2deb92be5db244984562426246e0497ce712e
a13n20 <143044296+a13n20@users.noreply.github.com>
2024-02-21 11:52:22 -0800

    Update test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -1,4 +1,24 @@
-#!/bin/bash
+name: Build C++
 
-g++ main.cpp --std=c++17 -o Palette
+on:
+  push:
+    branches: [ main ]
+  pull_request:
+    branches: [ main ]
+
+jobs:
+  install:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Install dependencies
+        run: |
+          sudo apt-get update
+          sudo apt-get install -y -f build-essential g++ cmake
+  build:
+    needs: install
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Build project
+        run: g++ main.cpp --std=c++17 -o Palette
 ./Palette 000 afC940 876 5104 FEFED 9964Ba 00G000
